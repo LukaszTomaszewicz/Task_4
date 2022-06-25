80a07c1d676a6794893995e8e8fedbb57c4f5b1f
LukaszTomaszewicz <102756308+LukaszTomaszewicz@users.noreply.github.com>
2022-06-25 14:47:37 +0200

    Add hammingDistance function, update readFile function

--- functions.cpp
+++ functions.cpp
@@ -5,13 +5,41 @@ void readFile(string path1, string path2)
     uint fileSize1{0};
     uint fileSize2{0};
     bool diff { 0 };
+    
+    fileSize1 = file_size(path1);
+    fileSize2 = file_size(path2);
 
     if(fileSize2 != fileSize1)
     {
-      // provided files have different size
+      openLog();
+      writeLog("Size of provided files is different!");
+      closeLog();
     }
     else
     {
-      // provided files have same size
+      openLog();
+      writeLog("Processing...");
+      if(diff)
+      {
+          // function to print ber
+      }
+      else
+      {
+          writeLog("No differences found");
+      }
+      closeLog();
     }
+}
+
+// from example project
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2; // XOR
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
 }
