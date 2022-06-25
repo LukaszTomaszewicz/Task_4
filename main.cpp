d2bc3ed1dcbdfd7494c9da1b6f1f004164715d42
LukaszTomaszewicz <102756308+LukaszTomaszewicz@users.noreply.github.com>
2022-06-25 14:31:45 +0200

    use readfile function in main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@ int main(int argc, char * argv[])
 {
     if(argc >3 && argc < 2)
     {
+        cerr << " Provided wrong number of args! " << endl;
         exit(1);
     }
+    readFile(argv[1], argv[2]);
+    
     return 0;
 }
