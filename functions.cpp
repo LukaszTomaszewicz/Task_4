5c8ad422c4f265675f48b7c5b39c566e06ea4cdf
LukaszTomaszewicz <102756308+LukaszTomaszewicz@users.noreply.github.com>
2022-06-25 14:26:45 +0200

    Create function.cpp

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+#include "functions.hpp"
+
+void readFile(string path1, string path2)
+{
+    uint fileSize1{0};
+    uint fileSize2{0};
+    bool diff { 0 };
+
+    if(fileSize2 != fileSize1)
+    {
+      // provided files have different size
+    }
+    else
+    {
+      // provided files have same size
+    }
+}
