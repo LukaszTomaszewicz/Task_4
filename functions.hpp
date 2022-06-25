af3764d71db7a4a68758af0a5493bd31335d0ac1
LukaszTomaszewicz <102756308+LukaszTomaszewicz@users.noreply.github.com>
2022-06-25 14:29:41 +0200

    Add libraries and function definition, use namespaces

--- functions.hpp
+++ functions.hpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <filesystem>
+
+// QOL
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::string;
+using std::ifstream;
+using std::fstream;
+using std::filesystem::file_size;
+
+void readFile(string path1, string path2);
